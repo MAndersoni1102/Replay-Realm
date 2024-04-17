5b767192a49cc7b7187a092618441f353c0754db
Reputation <kylekirby86@gmail.com>
2024-04-16 18:41:25 -0600

    Console and Game HTML's added, Modal Added, and A BUNCH OF OTHER STUFF we talked about.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,25 @@
+// Modal Start
+var modal = document.getElementById('myModal');
 
- 
+var btn = document.getElementById("openModalBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+// Modal End
+
  //This is the section that adds the tax rate to the calculated total in the cart
  fetch('https://fakestoreapi.com/products/category/electronics')
  .then(res=>res.json())
